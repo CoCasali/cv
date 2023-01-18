cabf18cf3414c7bcc7c6e9a29ad960e69b7ad9a6
CoCasali <corentin.casali@gmail.com>
2023-01-18 14:25:05 +0100

    new image + CV

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+// import ME from '../../assets/corentin_header.png'
 import ME from '../../assets/corentin_header.png'
 import HeaderSocial from './HeaderSocial'
 
