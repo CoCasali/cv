1ef7d98ff6dee003e4aa82a7726083eae36cba1a
CoCasali <corentin.casali@gmail.com>
2023-01-25 14:21:36 +0100

    change CV and title html

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CV from '../../assets/CV_CorentinCasali.pdf'
+import { FaGithub } from 'react-icons/fa'
 
 const CTA = () => {
   return (
     <div className='cta'>
         <a href={CV} download className='btn'>Download CV</a>
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        <a href="https://github.com/CoCasali/" className='btn btn-primary'> <FaGithub/> GitHub</a>
     </div>
   )
 }
